a276c607802b3405a1effe6fa30a6819c1e7b852
shikha22 <shikha2022r2@gmail.com>
2025-08-02 23:51:20 +0530

    completed

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -10,18 +10,18 @@ const Footer = () => {
     <footer className={isAuthorized ? "footerShow" : "footerHide"}>
       <div>&copy; All Rights Reserved By CodeWithZeeshu.</div>
       <div>
-        <Link to={"https://www.facebook.com/profile.php?id=100030535123397"} target="_blank">
+        {/* <Link to={"https://www.facebook.com/profile.php?id=100030535123397"} target="_blank">
           <FaFacebookF />
         </Link>
         <Link to={"https://www.youtube.com/@CodeWithZeeshu"} target="_blank">
           <FaYoutube />
-        </Link>
-        <Link to={"https://www.youtube.com/@CodeWithZeeshu"} target="_blank">
+        </Link> */}
+        {/* <Link to={"https://www.youtube.com/@CodeWithZeeshu"} target="_blank">
           <FaLinkedin />
-        </Link>
-        <Link to={"https://www.instagram.com/z_4_zeeshuuu/"} target="_blank">
+        </Link> */}
+        {/* <Link to={"https://www.instagram.com/z_4_zeeshuuu/"} target="_blank">
           <RiInstagramFill />
-        </Link>
+        </Link> */}
       </div>
     </footer>
   );
